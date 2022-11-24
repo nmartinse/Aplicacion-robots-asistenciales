8f37a558a092229cabf2ced4d837af21455ac6e2
Archerd6 <archerd@uma.es>
2022-11-24 16:18:50 +0100

    Anadido Task Manager de clase

--- Back end/app.py
+++ Back end/app.py
@@ -1,10 +1,62 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # 3 barras porque es un path relativo. SI fuera absoluto, serían 4 barras
+with app.app_context():
+    db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return '<Task %r' % self.id
+
+
+@app.route('/', methods=['POST', 'GET']) ## Indica qué hacer cuando un usuario llegua a la pagina principal
 def index():
-    return "Hello, World!"
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            return "Problem while insereting in the DB"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all() # Este método nos devuelve todos los elementos de la tabla de la base de datos
+        return render_template('index.html', tasks=tasks) # El primer tasks es el que usaremos para representar las tareas en el index.html
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Error while deleting the task" + id
+
+@app.route('/update/<int:id>', methods=['POST','GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue when updating the task " + id
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
