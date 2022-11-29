6b4686ced2d48a5563b21e7174403165f1641d8c
nmartin <nmartins@uma.es>
2022-11-29 20:55:48 +0100

    intento

--- Back end/app.py
+++ Back end/app.py
@@ -11,7 +11,6 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
     def __repr__(self):
         return '<Task %r' % self.id
 
