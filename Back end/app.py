4a10e62912d008170122ed3b1cf98206c21e1bb8
nmartin <nmartins@uma.es>
2022-12-11 02:18:59 +0100

    actualizacion web 🖥

--- Back end/app.py
+++ Back end/app.py
@@ -25,30 +25,30 @@ def index():
 def index_alt():
     return render_template('index.html') # Renderizar index.html
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
 
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return "Error while deleting the task" + id
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return "Error while deleting the task" + id
 
-@app.route('/update/<int:id>', methods=['POST','GET'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
+# @app.route('/update/<int:id>', methods=['POST','GET'])
+# def update(id):
+#     task = Todo.query.get_or_404(id)
+#     if request.method == 'POST':
+#         task.content = request.form['content']
         
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue when updating the task " + id
-    else:
-        return render_template('update.html', task=task)
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return "There was an issue when updating the task " + id
+#     else:
+#         return render_template('update.html', task=task)
 
 @app.route('/Interfaz_tecnico.html', methods=['POST','GET'])   # Redireccion a Tecnico
 def tecnico():
@@ -56,11 +56,31 @@ def tecnico():
 
 @app.route('/interfaz_encargado.html', methods=['POST','GET'])   # Redireccion a Encargado
 def encargado():
-    return render_template('interfaz_encargado.html')
+    tareas = Todo.query.order_by(Todo.content).all() # para presentar las tareas por orden alfabetico
+    return render_template('tareas_encargado.html', tareas = tareas)
 
-@app.route('/interfaz_tareas.html', methods=['POST','GET'])   # Redireccion a formulario tareas
+@app.route('/interfaz_tareas.html', methods=['POST','GET'])   # Redireccion a formulario creacion tareas
 def tareas():
-    return render_template('interfaz_tareas.html')
+    # if request.method == "POST":
+    #     tarea_nombre = request.form['nombre']
+    #     new_tarea = Todo(content=tarea_nombre)
+
+    #     try:
+    #         db.session.add(new_tarea)
+    #         db.session.commit()
+    #         return redirect('/')
+            
+    # else:
+        return render_template('interfaz_tareas.html')
+
+@app.route('/asig_tareas/<int:id>', methods=['POST','GET'])   # Redireccion a formulario asignacion tareas
+def asig_tareas(id):
+    tarea = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+         task.content = request.form['content']
+    else:
+        return render_template('tareas_encargado.html', tarea=tarea)
 
 if __name__ == "__main__":
     app.run(debug=True)
