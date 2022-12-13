67bed2bb244aa692931ea0881f07d38ac50ee984
Archerd6 <archerd@uma.es>
2022-12-13 21:05:11 +0100

    Actualizado vista de tecnico 📇 con base de datos v5

--- Back end/app.py
+++ Back end/app.py
@@ -13,10 +13,18 @@ class Todo(db.Model):
     completed = db.Column(db.Integer, default=0)
     priority = db.Column(db.Integer, default=0)         # Nueva tabla
     descripcion = db.Column(db.String(200))             # Descripcion
+    atributos = db.Column(db.String(800))               # Atributos
 
-    def __repr__(self):
-        return '<Task %r' % self.id
+class Robo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(200), nullable=False)
 
+
+@app.route('/test', methods=['POST', 'GET']) ## Indica qué hacer cuando un usuario llega a la pagina principal
+def test():
+
+    tasks = Todo.query.order_by(Todo.content).all() # Todas las tareas
+    return str(tasks[-2].id)                        # La ultima tarea
 
 @app.route('/', methods=['POST', 'GET']) ## Indica qué hacer cuando un usuario llega a la pagina principal
 def index():
@@ -42,7 +50,8 @@ def index_alt():
 @app.route('/Interfaz_tecnico.html', methods=['POST','GET'])     # Redireccion a Tecnico
 def tecnico():
     tasks = Todo.query.order_by(Todo.id).all() # Este método nos devuelve todos los elementos de la tabla de la base de datos
-    return render_template('Interfaz_tecnico.html', tasks=tasks) # Presentar las tareas
+    rob = Robo.query.order_by(Robo.id).all()
+    return render_template('Interfaz_tecnico.html', tasks=tasks, rob=rob) # Presentar las tareas
     # if request.method == 'POST':
     #     task_content = "Prueba de texto"
     #     new_task = Todo(content=task_content)
@@ -102,6 +111,7 @@ def update_tarea(id):
         try:
             task.content = request.form['nombre']
             task.descripcion = request.form['descripcion']
+            task.atributos = request.form['atributos']
             db.session.commit()
             return redirect('/Interfaz_tecnico.html')
         except:
@@ -117,8 +127,9 @@ def new_tarea(id):
 
         try:
             task_content = request.form['nombre']
-            task_content = request.form['nombre']
-            new_task = Todo(content=task_content)
+            task_descrip = request.form['descripcion']
+            task_atribut = request.form['atributos']
+            new_task = Todo(content=task_content, descripcion=task_descrip, atributos=task_atribut)
             db.session.add(new_task)
             db.session.commit()
             return redirect('/Interfaz_tecnico.html')
@@ -150,6 +161,44 @@ def asignar(id):
             return "There was an issue when updating the task " + id
     else:
         return render_template('/asignar_tarea.html', task=task)
+
+
+@app.route('/nuevo_robot')
+def nuevo_robot():
+    rob = Robo.query.order_by(Robo.id).all() # Todas las tareas
+    
+    new_rob = Robo(nombre="Wally")
+    try:
+        db.session.add(new_rob)
+        db.session.commit()
+        return redirect('/Interfaz_tecnico.html')
+    except:
+        return "Problem while insereting in the DB"
+
+
+@app.route('/delete_robot')
+def delete_robot():
+    rob = Robo.query.order_by(Robo.id).all() # Todas las tareas
+    id= rob[-1].id                           # La ultima tarea
+
+    if(id > 1):
+        task_to_delete = Robo.query.get_or_404(id)
+
+        try:
+            db.session.delete(task_to_delete)
+            db.session.commit()
+            return redirect('/Interfaz_tecnico.html')
+        except:
+            return "Error while deleting the task" + id
+    return redirect('/Interfaz_tecnico.html')
+
+    # rob_to_delete = Robo.query.get_or_404(id)
+    # try:
+    #     db.session.delete(id)
+    #     db.session.commit()
+    #     return redirect('/Interfaz_tecnico.html')
+    # except:
+    #     return "Error while deleting the task" + id
 
 if __name__ == "__main__":
     app.run(debug=True)
