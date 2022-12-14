21fd070fa2b06722e7ce6cf15d300068dee342e4
Archerd6 <archerd@uma.es>
2022-12-14 20:26:14 +0100

    Asignar tareas 0.8

--- Back end/app.py
+++ Back end/app.py
@@ -161,7 +161,14 @@ def encargado():
 @app.route('/asignar/<int:id>', methods=['POST','GET'])      # Redireccion a formulario asignar tareas
 def asignar(id):
     task = Todo.query.get_or_404(id)
-    rob = Robo.query.order_by(Robo.id).all() # Todas las tareas
+    # rob = Robo.query.order_by(Robo.id).all() # Todas las tareas
+    cars = ["Ford", "Volvo", "BMW"] 
+    atributosi = task.atributos
+
+    atributosii = atributosi.split(",")
+
+    atributosii.pop(0)
+
     if request.method == 'POST':
 
         try:
@@ -170,7 +177,7 @@ def asignar(id):
         except:
             return "There was an issue when updating the task " + id
     else:
-        return render_template('/asignar_tarea.html', task=task, rob=rob)
+        return render_template('/asignar_tarea.html', task=task, cars=cars, taskinfo=atributosii)
 
 
 @app.route('/nuevo_robot')
